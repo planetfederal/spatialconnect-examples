9f049231f83a055ad7849b129d82ef2b279e4ed6
Wes Richardet <wes@tetriscodes.com>
2016-01-20 16:57:22 -0600

    Adding button for add feature

--- web/app/js/components/location.js
+++ web/app/js/components/location.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var React = require('react');
-var sc = require('spatialconnect-js');
+var sc = require('spatialconnect');
 
 var Location = React.createClass({
   getInitialState: function() {
--- web/app/js/components/map.js
+++ web/app/js/components/map.js
@@ -1,8 +1,11 @@
 'use strict';
+/*global ol*/
 
 var React = require('react');
 var Location = require('./location');
 var GPS = require('./gps');
+var ol = require('openlayers');
+var sc = require('spatialconnect');
 
 class Map extends React.Component {
   componentDidMount() {
@@ -11,16 +14,31 @@ class Map extends React.Component {
     var elem = React.findDOMNode(me);
     map.setTarget(elem);
   }
-
+  addFeature() {
+    var map = this.props.map;
+    var coord = map.getView().getCenter();
+    var feature; //TODO
+    featureObs.onNext({action:ADD,value:feature});
+  }
   render() {
     return (
-      <div className="row">
-        <div className="col-xs-8">
-          <GPS></GPS>
+      <div>
+        <div className="row">
+          <div className="col-xs-2">
+            <GPS></GPS>
+          </div>
+          <div className="col-xs-2">
+            <button onClick={this.addFeature}>
+              Add Feature
+            </button>
+          </div>
         </div>
+      <div className="row">
         <div>
           <div ref="map" id="map"></div>
         </div>
+      </div>
+
       </div>
     );
   }
